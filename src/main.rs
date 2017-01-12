7d966117b42383264f9b2a25430f63e18e9e6c59
Bobby Lei <bobl53@gmail.com>
2017-01-12 09:17:54 +0800

    adding a game loop

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,31 @@ use tcod::input::Key;
 use tcod::input::{KeyCode};
 use tcod::input::KeyCode::{Up, Down, Left, Right, Escape, Enter};
 
+fn render(con: &mut RootConsole) {
+    con.clear();
+    con.put_char(40, 25, '@', BackgroundFlag::Set);
+    con.flush();
+}
+
 fn main() {
     let mut con = RootConsole::initializer().size(80, 50).title("libtcod Rust tutorial").init();
     let mut exit = false;
+
+    // render
+    render(&mut con);
+
+    // our game loop
     while !(con.window_closed() || exit) {
-        con.clear();
-        con.put_char(40, 25, '@', BackgroundFlag::Set);
-        con.flush();
+        // wait for user input
         let keypress = con.wait_for_keypress(true);
+
+        // update game state
         match keypress {
-            Key { code: Escape, .. } => exit = true,
-            _ => {}
+            Key {code: Escape, .. } => exit = true,
+            _ => {},
         }
+
+        //render
+        render(&mut con);
     }
 }
