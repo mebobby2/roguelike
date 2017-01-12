710a5ff56af99bdc78e909d8a8b37e0c06a782f1
Bobby Lei <bobl53@gmail.com>
2017-01-12 15:49:34 +0800

    making the character move up

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,21 @@ use tcod::input::Key;
 use tcod::input::{KeyCode};
 use tcod::input::KeyCode::{Up, Down, Left, Right, Escape, Enter};
 
-fn render(con: &mut RootConsole) {
+fn render(con: &mut RootConsole, x: i32, y: i32) {
     con.clear();
-    con.put_char(40, 25, '@', BackgroundFlag::Set);
+    con.put_char(x, y, '@', BackgroundFlag::Set);
     con.flush();
 }
 
 fn main() {
+    let mut charX = 40i32;
+    let mut charY = 25i32;
+
     let mut con = RootConsole::initializer().size(80, 50).title("libtcod Rust tutorial").init();
     let mut exit = false;
 
     // render
-    render(&mut con);
+    render(&mut con, charX, charY);
 
     // our game loop
     while !(con.window_closed() || exit) {
@@ -26,10 +29,15 @@ fn main() {
         // update game state
         match keypress {
             Key {code: Escape, .. } => exit = true,
-            _ => {},
+            Key {code: Up, .. } => {
+                if charY >= 1 {
+                    charY -= 1;
+                }
+            },
+            _ => {}
         }
 
         //render
-        render(&mut con);
+        render(&mut con, charX, charY);
     }
 }
