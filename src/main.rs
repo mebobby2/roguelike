1cd8b7bd40a050525e701c474f33e3522fd18cc7
Bobby Lei <bobl53@gmail.com>
2017-01-13 08:42:10 +0800

    refactored using point struct

--- src/main.rs
+++ src/main.rs
@@ -7,26 +7,30 @@ use tcod::input::{KeyCode};
 use tcod::input::KeyCode::{Up, Down, Left, Right, Escape, Enter};
 use rand::Rng;
 
-fn render(con: &mut RootConsole, x: i32, y: i32, dogX: i32, dogY: i32) {
+#[derive(Copy, Clone)]
+struct Point {
+  x: i32,
+  y: i32
+}
+
+fn render(con: &mut RootConsole, c_point: Point, d_point: Point) {
     con.clear();
-    con.put_char(x, y, '@', BackgroundFlag::Set);
-    con.put_char(dogX, dogY, 'd', BackgroundFlag::Set);
+    con.put_char(c_point.x, c_point.y, '@', BackgroundFlag::Set);
+    con.put_char(d_point.x, d_point.y, 'd', BackgroundFlag::Set);
     con.flush();
 }
 
 fn main() {
     let conX = 80i32;
     let conY = 50i32;
-    let mut charX = 40i32;
-    let mut charY = 25i32;
-    let mut dogX = 10i32;
-    let mut dogY = 10i32;
+    let mut char_point = Point { x: 40, y: 25 };
+    let mut dog_point = Point { x: 10, y: 10 };
 
     let mut con = RootConsole::initializer().size(conX, conY).title("libtcod Rust tutorial").init();
     let mut exit = false;
 
     // render
-    render(&mut con, charX, charY, dogX, dogY);
+    render(&mut con, char_point, dog_point);
 
     // our game loop
     while !(con.window_closed() || exit) {
@@ -34,42 +38,42 @@ fn main() {
         let keypress = con.wait_for_keypress(true);
 
         // update game state
-        let offset_x = rand::thread_rng().gen_range(0, 3i32) - 1;
-        if (dogX + offset_x) > 0 && (dogX + offset_x) < (conX - 1) {
-            dogX += offset_x;
-        }
-
-        let offset_y = rand::thread_rng().gen_range(0, 3i32) - 1;
-        if (dogY + offset_y) > 0 && (dogY + offset_y) < (conY - 1) {
-            dogY += offset_y;
-        }
-
         match keypress {
             Key {code: Escape, .. } => exit = true,
             Key {code: Up, .. } => {
-                if charY >= 1 {
-                    charY -= 1;
+                if char_point.y >= 1 {
+                    char_point.y -= 1;
                 }
             },
             Key {code: Down, .. } => {
-                if charY < (conY - 1) {
-                    charY += 1;
+                if char_point.y < (conY - 1) {
+                    char_point.y += 1;
                 }
             },
             Key {code: Left, .. } => {
-                if charX >= 1 {
-                    charX -= 1
+                if char_point.x >= 1 {
+                    char_point.x -= 1
                 }
             },
             Key {code: Right, .. } => {
-                if charX < (conX -1) {
-                    charX += 1
+                if char_point.x < (conX -1) {
+                    char_point.x += 1
                 }
             },
             _ => {}
         }
 
+        let offset_x = rand::thread_rng().gen_range(0, 3i32) - 1;
+        if (dog_point.x + offset_x) > 0 && (dog_point.x + offset_x) < (conX - 1) {
+            dog_point.x += offset_x;
+        }
+
+        let offset_y = rand::thread_rng().gen_range(0, 3i32) - 1;
+        if (dog_point.y + offset_y) > 0 && (dog_point.y + offset_y) < (conY - 1) {
+            dog_point.y += offset_y;
+        }
+
         //render
-        render(&mut con, charX, charY, dogX, dogY);
+        render(&mut con, char_point, dog_point);
     }
 }
