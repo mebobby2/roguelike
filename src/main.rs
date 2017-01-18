ead77b2fee3b6b65e2cd51dfd7d7fae9c557b5bc
Bobby Lei <bobl53@gmail.com>
2017-01-18 08:35:19 +0800

    rendering our character last - essentially using layers during the rendering pipeline

--- src/main.rs
+++ src/main.rs
@@ -11,16 +11,17 @@ use roguelike::traits::Updates;
 use roguelike::character::Character;
 use roguelike::npc::NPC;
 
-fn render(con: &mut RootConsole, objs: &Vec<Box<Updates>>) {
+fn render(con: &mut RootConsole, npcs: &Vec<Box<Updates>>, c: Character) {
     con.clear();
-    for i in objs.iter() {
+    for i in npcs.iter() {
         i.render(con);
     }
+    c.render(con);
     con.flush();
 }
 
-fn update(objs: &mut Vec<Box<Updates>>, keypress: tcod::input::Key, game: Game) {
-    for i in objs.iter_mut() {
+fn update(npcs: &mut Vec<Box<Updates>>, keypress: tcod::input::Key, game: Game) {
+    for i in npcs.iter_mut() {
         i.update(keypress, game);
     }
 }
@@ -29,12 +30,14 @@ fn main() {
     let mut game = Game { exit: false, window_bounds: Bound { min: Point { x: 0, y: 0}, max: Point { x: 79, y: 49 } }};
     let mut con = RootConsole::initializer().size(game.window_bounds.max.x, game.window_bounds.max.y).title("libtcod Rust tutorial").init();
 
-    let c = Box::new(Character::new(40, 25, '@')); //box Character::new(40, 25, '@') as Box<Updates>;
-    let d = Box::new(NPC::new(10, 10, 'd')); //box NPC::new(10, 10, 'd') as Box<Updates>;
-    let mut objs: Vec<Box<Updates>> = vec![c,d];
+    let mut c = Character::new(40, 25, '@');
+    let d = Box::new(NPC::new(10, 10, 'd'));
+    let ct = Box::new(NPC::new(40, 25, 'c'));
+
+    let mut npcs: Vec<Box<Updates>> = vec![d,ct];
 
     // render
-    render(&mut con, &objs);
+    render(&mut con, &npcs, c);
 
     // our game loop
     while !(con.window_closed() || game.exit) {
@@ -45,8 +48,8 @@ fn main() {
             Key {code: Escape, .. } => game.exit = true,
             _ => {}
         }
-        update(&mut objs, keypress, game);
+        update(&mut npcs, keypress, game);
 
-        render(&mut con, &objs);
+        render(&mut con, &npcs, c);
     }
 }
