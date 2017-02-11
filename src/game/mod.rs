cf833e9bce6033cae266a977fbffbb4042d4ac6b
Bobby Lei <bobl53@gmail.com>
2017-02-11 12:42:13 +0800

    attached the right pane

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,15 +1,15 @@
 extern crate tcod;
 
-use self::tcod::RootConsole;
 use self::tcod::input::Key;
 use util::{Bound, Point};
-use rendering::{RenderingComponent, TcodRenderingComponent};
+use rendering::{RenderingComponent, TcodRenderingComponent, WindowComponent, TcodStatsWindowComponent};
 use actor::Actor;
 
 pub struct Game {
     pub exit: bool,
     pub window_bounds: Bound,
-    pub rendering_component: Box<RenderingComponent>
+    pub rendering_component: Box<RenderingComponent>,
+    pub stats_window: Box<WindowComponent>
 }
 
 static mut LAST_KEYPRESS: Option<Key> = None;
@@ -17,22 +17,22 @@ static mut CHAR_LOCATION: Point = Point { x: 40, y: 25 };
 
 impl Game {
   pub fn new() -> Game {
-   let bound = Bound {
-      min: Point { x: 0, y: 0 },
-      max: Point { x: 79, y: 49}
-    };
-    let con = RootConsole::initializer().size(bound.max.x, bound.max.y).title("Roguelike!").init();
+    let total_bounds = Bound::new(0, 0, 99, 61);
+    let stats_bounds = Bound::new(79, 0, 99, 49);
 
-    let rc: Box<RenderingComponent> = Box::new(TcodRenderingComponent::new(con));
+    let rc: Box<RenderingComponent> = Box::new(TcodRenderingComponent::new(total_bounds));
+    let sw: Box<WindowComponent> = Box::new(TcodStatsWindowComponent::new(stats_bounds));
     Game {
       exit: false,
-      window_bounds: bound,
-      rendering_component: rc
+      window_bounds: total_bounds,
+      rendering_component: rc,
+      stats_window: sw
     }
   }
 
   pub fn render(&mut self, npcs: &Vec<Box<Actor>>, c: &Actor) {
     self.rendering_component.before_render_new_frame();
+    self.rendering_component.attach_window(&mut self.stats_window);
     for i in npcs.iter() {
         i.render(&mut self.rendering_component);
     }
--- src/rendering/mod.rs
+++ src/rendering/mod.rs
@@ -1,9 +1,9 @@
 extern crate tcod;
 
-use self::tcod::{Console, RootConsole, BackgroundFlag};
+use self::tcod::{Console, RootConsole, BackgroundFlag, Color, OffscreenConsole, TextAlignment};
 use self::tcod::input::Key;
 
-use util::{Point};
+use util::{Point, Bound};
 
 pub trait RenderingComponent {
     fn before_render_new_frame(&mut self);
@@ -11,6 +11,7 @@ pub trait RenderingComponent {
     fn after_render_new_frame(&mut self);
     fn wait_for_keypress(&mut self) -> Key;
     fn window_closed(&mut self) -> bool;
+    fn attach_window(&mut self, window: &mut Box<WindowComponent>);
 }
 
 pub struct TcodRenderingComponent {
@@ -18,7 +19,9 @@ pub struct TcodRenderingComponent {
 }
 
 impl TcodRenderingComponent {
-    pub fn new(console: RootConsole) -> TcodRenderingComponent {
+    pub fn new(bounds: Bound) -> TcodRenderingComponent {
+        let console = RootConsole::initializer().size(bounds.max.x + 1, bounds.max.y + 1).title("Roguelike!").init();
+
         TcodRenderingComponent {
             console: console
         }
@@ -45,4 +48,69 @@ impl RenderingComponent for TcodRenderingComponent {
     fn window_closed(&mut self) -> bool {
         self.console.window_closed()
     }
+
+    fn attach_window(&mut self, window: &mut Box<WindowComponent>) {
+        window.clear();
+        window.print_message(0, 0, TextAlignment::Left, "Sup foo!");
+        window.print_message(0, 1, TextAlignment::Left, "Nothing fool!");
+        let bounds = window.get_bounds();
+        let console = window.get_console();
+
+        tcod::console::blit(&*console,
+                            (0, 0),
+                            (bounds.max.x + 1, bounds.max.y + 1),
+                            &mut self.console,
+                            (bounds.min.x, bounds.min.y),
+                            1.0,
+                            1.0);
+
+        // tcod::console::blit(con, (0, 0), (SCREEN_WIDTH, SCREEN_HEIGHT),
+        //                 root, (0, 0),
+        //                 1.0, 1.0);
+    }
+}
+
+pub trait WindowComponent {
+    fn get_bounds(&self) -> Bound;
+    fn get_bg_color(&self) -> Color;
+    fn get_console(&mut self) -> &mut OffscreenConsole;
+
+    fn clear(&mut self) {
+        let color = self.get_bg_color();
+        let mut console = self.get_console();
+        console.set_default_background(color);
+        console.clear();
+    }
+
+    fn print_message(&mut self, x: i32, y: i32, alignment: TextAlignment, text: &str) {
+        let mut console = self.get_console();
+        console.print_ex(x, y, BackgroundFlag::Set, alignment, text);
+    }
+}
+
+pub struct TcodStatsWindowComponent {
+    pub console: OffscreenConsole,
+    pub background_color: Color,
+    bounds: Bound
+}
+
+impl TcodStatsWindowComponent {
+    pub fn new(bounds: Bound) -> TcodStatsWindowComponent {
+        let height = bounds.max.y - bounds.min.y + 1;
+        let width = bounds.max.x - bounds.min.x + 1;
+        let console = OffscreenConsole::new(width, height);
+
+        let red = Color::new(255u8, 0u8, 0u8);
+        TcodStatsWindowComponent {
+            console: console,
+            background_color: red,
+            bounds: bounds
+        }
+    }
+}
+
+impl WindowComponent for TcodStatsWindowComponent {
+    fn get_console(&mut self) -> &mut OffscreenConsole { &mut self.console }
+    fn get_bounds(&self) -> Bound { self.bounds }
+    fn get_bg_color(&self) -> Color { self.background_color }
 }
